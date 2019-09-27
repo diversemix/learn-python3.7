49b65010480fa06b742045502e9cd644f17a664b
Peter Hooper <diversemix@gmail.com>
2019-09-27 13:50:19 +0100

    Change tests

--- src/test/test_service.py
+++ src/test/test_service.py
@@ -1,14 +1,17 @@
+import pytest
 from ..widget.service import WidgetService
 
 # Expect DeprecationWarning, see
 # https://github.com/pallets/jinja/issues/969
 
-def test_service_has_controller(mocker):
-    config = mocker.Mock()
+@pytest.fixture
+def config(tmpdir):
+  return { "database_uri" : tmpdir + "/tmp.sqlite3"}
+
+def test_service_has_controller(config):
     widget_service = WidgetService(config)
     assert widget_service.get_controller() is not None
 
-def test_service_has_api(mocker):
-    config = mocker.Mock()
+def test_service_has_api(config):
     widget_service = WidgetService(config)
     assert widget_service.get_api() is not None
